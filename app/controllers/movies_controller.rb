c097d16bdd05b1d715ff8915e8398a80f4a11122
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-05-12 21:51:41 +0000

    eighty four pr

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,16 @@
 class MoviesController < ApplicationController
+  def new
+    @the_movie = Movie.new
+    
+    render template: "movies/new.html.erb"
+  end
+
+  def edit
+    @the_movie = Movie.where(id: params.fetch(:id)).first
+    
+    render template: "movies/edit.html.erb"
+  end
+
   def index
     matching_movies = Movie.all
 
@@ -19,16 +31,21 @@ class MoviesController < ApplicationController
   end
 
   def create
-    the_movie = Movie.new
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
-    the_movie.released = params.fetch("query_released", false)
+    @the_movie = Movie.new
+    @the_movie.title = params.fetch("query_title")
+    @the_movie.description = params.fetch("query_description")
+    @the_movie.released = params.fetch("query_released", false)
 
-    if the_movie.valid?
-      the_movie.save
+    if @the_movie.valid?
+      @the_movie.save
       redirect_to("/movies", { :notice => "Movie created successfully." })
-    else
-      redirect_to("/movies", { :alert => the_movie.errors.full_messages.to_sentence })
+    else 
+      #cookies[:title] = params.fetch("query_title")
+      #cookies[:description] = params.fetch("query_description")
+      #cookies[:released] = params.fetch("query_released")
+      #redirect_to("/movies/new", { :alert => the_movie.errors.full_messages.to_sentence })
+
+    render template: "movies/new"
     end
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
-class Book < ApplicationRecord
+class Book < ApplicationRecord 
+   validates :title, presence: true
+   validates :description, presence: true
 end
