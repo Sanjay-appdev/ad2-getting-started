9895ed882c347a8b17de6af1cce8a5de8dd9e009
sanjay5649 <stuwrkr-smaru7@mx.uillinois.edu>
2023-05-12 07:40:54 +0000

    movies and books table create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  resources :books
+  # Routes for the Movie resource:
+
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
